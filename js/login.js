ddab1a0158ce6f106cc1064efd2c4f92810caca6
Magnus <magndut@gmail.com>
2023-11-15 12:56:59 +0100

    Made async

--- js/login.js
+++ js/login.js
@@ -1,26 +1,23 @@
-
 var loginForm = document.getElementById('loginForm');
 
-
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        
-        var email = document.getElementById('email').value;
-        var password = document.getElementById('password').value;
-        
-        fetch('http://localhost:3000/users?email=' + email + '&password=' + password)
-        .then(response => response.json())
-        .then(data => {
-            if (data.length > 0) {
-                sessionStorage.setItem('email', email);
-                console.log('User logged in');
-                window.location.href = 'index.html';
-            } else {
-                console.log('Invalid credentials');
-                alert('invalid credentials')
-            }
-        })
-        .catch(error => console.error(error));
-    });
-
-
+loginForm.addEventListener('submit', async function(event) {
+    event.preventDefault();
+    
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    
+    try {
+        const response = await fetch('http://localhost:3000/users?email=' + email + '&password=' + password);
+        const data = await response.json();
+        if (data.length > 0) {
+            sessionStorage.setItem('email', email);
+            console.log('User logged in');
+            window.location.href = 'index.html';
+        } else {
+            console.log('Invalid credentials');
+            alert('invalid credentials')
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
--- js/signup.js
+++ js/signup.js
@@ -1,25 +1,27 @@
-document.getElementById('signupForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.getElementById('signupForm').addEventListener('submit', async function(event) {
+  event.preventDefault();
    
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var confirmPassword = document.getElementById('confirmPassword').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  var confirmPassword = document.getElementById('confirmPassword').value;
 
-    if (password !== confirmPassword) {
-        alert('Passwords do not match');
-        return; 
-    }
+  if (password !== confirmPassword) {
+    alert('Passwords do not match');
+    return; 
+  }
    
   //TODO: password req
-    fetch('http://localhost:3000/users', {
+  try {
+    const response = await fetch('http://localhost:3000/users', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ email: email, password: password, confirmPassword: confirmPassword })
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 });
-
